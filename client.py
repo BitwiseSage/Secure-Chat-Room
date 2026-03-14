1f7abf59fcc068ede6669b7bbe482e216468a027
Arfan Asgar <md17arfan@gmail.com>
2026-03-14 09:46:02 +0530

    Implemented encrypted secret messaging feature

--- client.py
+++ client.py
@@ -1,106 +1,135 @@
 import socket
 import threading
+import ssl
 import os
 from protocol import send_json, recv_json
+from encryption_utils import encrypt_message, decrypt_message
 
-HOST="127.0.0.1"
-PORT=5000
+HOST = "127.0.0.1"
+PORT = 5000
 
-def receive(sock):
+
+def receive_messages(sock):
 
     while True:
+        try:
 
-        data=recv_json(sock)
+            data = recv_json(sock)
 
-        if not data:
+            if not data:
+                break
+
+            msg_type = data.get("type")
+
+            if msg_type == "message":
+                print(f"[{data['sender']}]: {data['content']}")
+
+            elif msg_type == "private":
+                print(f"[PRIVATE from {data['from']}]: {data['content']}")
+
+            elif msg_type == "secret":
+
+                decrypted = decrypt_message(data["content"])
+                print(f"[SECRET from {data['from']}]: {decrypted}")
+
+            elif msg_type == "info":
+                print(f"[INFO]: {data['message']}")
+
+            elif msg_type == "file":
+
+                filename = "received_files/" + data["filename"]
+                filesize = data["filesize"]
+
+                remaining = filesize
+
+                with open(filename, "wb") as f:
+
+                    while remaining > 0:
+                        chunk = sock.recv(min(4096, remaining))
+                        f.write(chunk)
+                        remaining -= len(chunk)
+
+                print(f"[FILE RECEIVED]: {filename}")
+
+        except:
             break
 
-        msg_type=data.get("type")
-
-        if msg_type=="message":
-            print(f"[{data['sender']}]: {data['content']}")
-
-        elif msg_type=="private":
-            print(f"[PRIVATE from {data['from']}]: {data['content']}")
-
-        elif msg_type=="info":
-            print("[INFO]",data["message"])
-
-        elif msg_type=="file":
-
-            filename="received_files/"+data["filename"]
-            filesize=data["filesize"]
-
-            remaining=filesize
-
-            with open(filename,"wb") as f:
-
-                while remaining>0:
-
-                    chunk=sock.recv(min(4096,remaining))
-
-                    f.write(chunk)
-
-                    remaining-=len(chunk)
-
-            print("[FILE RECEIVED]",filename)
 
 def start_client():
 
-    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((HOST,PORT))
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
 
-    threading.Thread(target=receive,args=(sock,),daemon=True).start()
+    raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    username=input("Enter username: ")
-    room=input("Enter room: ")
+    secure_sock = context.wrap_socket(raw_sock)
 
-    send_json(sock,{
-        "type":"join",
-        "username":username,
-        "room":room
+    secure_sock.connect((HOST, PORT))
+
+    threading.Thread(target=receive_messages, args=(secure_sock,), daemon=True).start()
+
+    username = input("Enter username: ")
+    room = input("Enter room: ")
+
+    send_json(secure_sock, {
+        "type": "join",
+        "username": username,
+        "room": room
     })
 
     while True:
 
-        msg=input()
+        msg = input()
 
         if msg.startswith("/private"):
 
-            _,to_user,content=msg.split(" ",2)
+            _, to_user, content = msg.split(" ", 2)
 
-            send_json(sock,{
-                "type":"private",
-                "to":to_user,
-                "content":content
+            send_json(secure_sock, {
+                "type": "private",
+                "to": to_user,
+                "content": content
+            })
+
+        elif msg.startswith("/secret"):
+
+            _, to_user, content = msg.split(" ", 2)
+
+            encrypted_content = encrypt_message(content)
+
+            send_json(secure_sock, {
+                "type": "secret",
+                "to": to_user,
+                "content": encrypted_content
             })
 
         elif msg.startswith("/file"):
 
-            _,filepath=msg.split(" ",1)
+            _, filepath = msg.split(" ", 1)
 
-            filesize=os.path.getsize(filepath)
-            filename=os.path.basename(filepath)
+            filesize = os.path.getsize(filepath)
+            filename = os.path.basename(filepath)
 
-            send_json(sock,{
-                "type":"file",
-                "filename":filename,
-                "filesize":filesize,
-                "room":room
+            send_json(secure_sock, {
+                "type": "file",
+                "filename": filename,
+                "filesize": filesize,
+                "room": room
             })
 
-            with open(filepath,"rb") as f:
-
-                while chunk:=f.read(4096):
-                    sock.sendall(chunk)
+            with open(filepath, "rb") as f:
+                while chunk := f.read(4096):
+                    secure_sock.sendall(chunk)
 
         else:
 
-            send_json(sock,{
-                "type":"message",
-                "room":room,
-                "content":msg
+            send_json(secure_sock, {
+                "type": "message",
+                "room": room,
+                "content": msg
             })
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     start_client()
--- server.py
+++ server.py
@@ -3,12 +3,50 @@ import threading
 import ssl
 from protocol import send_json, recv_json
 
-HOST="0.0.0.0"
-PORT=5000
+HOST = "0.0.0.0"
+PORT = 5000
 
-clients={}
-rooms={}
-lock=threading.Lock()
+clients = {}
+user_sockets = {}
+rooms = {}
+lock = threading.Lock()
+
+
+def broadcast_room(room, message, exclude=None):
+
+    if room in rooms:
+
+        for client in rooms[room]:
+
+            if client != exclude:
+
+                try:
+                    send_json(client, message)
+
+                except:
+                    remove_client(client)
+
+
+def remove_client(client):
+
+    with lock:
+
+        username = clients.get(client)
+
+        if username:
+
+            print(f"{username} disconnected")
+
+            del user_sockets[username]
+            del clients[client]
+
+            for room in rooms.values():
+
+                if client in room:
+                    room.remove(client)
+
+        client.close()
+
 
 def handle_client(client):
 
@@ -16,39 +54,104 @@ def handle_client(client):
 
         try:
 
-            data=recv_json(client)
+            data = recv_json(client)
 
             if not data:
                 break
 
-            if data["type"]=="message":
+            msg_type = data.get("type")
 
-                print("Message received")
+            if msg_type == "join":
+
+                username = data["username"]
+                room = data["room"]
+
+                with lock:
+
+                    clients[client] = username
+                    user_sockets[username] = client
+
+                    if room not in rooms:
+                        rooms[room] = []
+
+                    rooms[room].append(client)
+
+                send_json(client, {
+                    "type": "info",
+                    "message": f"Joined room {room}"
+                })
+
+                broadcast_room(room, {
+                    "type": "info",
+                    "message": f"{username} joined the room"
+                }, exclude=client)
+
+            elif msg_type == "message":
+
+                room = data["room"]
+                username = clients.get(client)
+
+                broadcast_room(room, {
+                    "type": "message",
+                    "sender": username,
+                    "content": data["content"]
+                })
+
+            elif msg_type == "private":
+
+                to_user = data["to"]
+                username = clients.get(client)
+
+                if to_user in user_sockets:
+
+                    send_json(user_sockets[to_user], {
+                        "type": "private",
+                        "from": username,
+                        "content": data["content"]
+                    })
+
+            elif msg_type == "secret":
+
+                to_user = data["to"]
+
+                if to_user in user_sockets:
+
+                    send_json(user_sockets[to_user], {
+                        "type": "secret",
+                        "from": clients[client],
+                        "content": data["content"]
+                    })
 
         except:
             break
 
-    client.close()
+    remove_client(client)
+
 
 def start_server():
 
-    context=ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain(certfile="cert.pem",keyfile="key.pem")
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
-    server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
 
-    server.bind((HOST,PORT))
-    server.listen()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    print("Server running with SSL")
+    server.bind((HOST, PORT))
+
+    server.listen(5)
+
+    print("Server running with SSL...")
 
     while True:
 
-        client,addr=server.accept()
+        client, addr = server.accept()
 
-        secure_client=context.wrap_socket(client,server_side=True)
+        secure_client = context.wrap_socket(client, server_side=True)
 
-        threading.Thread(target=handle_client,args=(secure_client,)).start()
+        print(f"Connection from {addr}")
 
-if __name__=="__main__":
+        threading.Thread(target=handle_client, args=(secure_client,)).start()
+
+
+if __name__ == "__main__":
     start_server()
