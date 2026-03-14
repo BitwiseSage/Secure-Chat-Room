055d35a1bdda55f0938d37dc839204229532f52e
Arfan Asgar <md17arfan@gmail.com>
2026-03-14 09:45:52 +0530

    Implemented basic TCP server skeleton

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
-print("Server launching in...3..2..1..BAHAHAHAHAA")
+import socket
+
+HOST = "0.0.0.0"
+PORT = 5000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print("Server started on port", PORT)
+
+while True:
+    client, addr = server.accept()
+    print("Client connected:", addr)
