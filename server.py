108291d0116c86105b3747ec202c621a08267fd3
Arfan Asgar <md17arfan@gmail.com>
2026-03-14 09:45:59 +0530

    Secure client server communication using SSL

--- server.py
+++ server.py
@@ -1,64 +1,29 @@
 import socket
 import threading
+import ssl
 from protocol import send_json, recv_json
 
-HOST = "0.0.0.0"
-PORT = 5000
+HOST="0.0.0.0"
+PORT=5000
 
-clients = {}
-user_sockets = {}
-rooms = {}
-lock = threading.Lock()
-
-def broadcast_room(room, message, exclude=None):
-    if room in rooms:
-        for client in rooms[room]:
-            if client != exclude:
-                send_json(client, message)
+clients={}
+rooms={}
+lock=threading.Lock()
 
 def handle_client(client):
+
     while True:
+
         try:
-            data = recv_json(client)
+
+            data=recv_json(client)
+
             if not data:
                 break
 
-            msg_type = data.get("type")
+            if data["type"]=="message":
 
-            if msg_type == "join":
-                username = data["username"]
-                room = data["room"]
-
-                with lock:
-                    clients[client] = username
-                    user_sockets[username] = client
-
-                    if room not in rooms:
-                        rooms[room] = []
-                    rooms[room].append(client)
-
-                send_json(client, {"type": "info", "message": f"Joined room {room}"})
-
-            elif msg_type == "message":
-                room = data["room"]
-                username = clients.get(client)
-
-                broadcast_room(room,{
-                    "type":"message",
-                    "sender":username,
-                    "content":data["content"]
-                })
-
-            elif msg_type == "private":
-                to_user = data["to"]
-                username = clients.get(client)
-
-                if to_user in user_sockets:
-                    send_json(user_sockets[to_user],{
-                        "type":"private",
-                        "from":username,
-                        "content":data["content"]
-                    })
+                print("Message received")
 
         except:
             break
@@ -66,17 +31,24 @@ def handle_client(client):
     client.close()
 
 def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
+
+    context=ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile="cert.pem",keyfile="key.pem")
+
+    server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    server.bind((HOST,PORT))
     server.listen()
 
-    print("Server running on port",PORT)
+    print("Server running with SSL")
 
     while True:
-        client, addr = server.accept()
-        print("Connection from",addr)
 
-        threading.Thread(target=handle_client,args=(client,)).start()
+        client,addr=server.accept()
 
-if __name__ == "__main__":
+        secure_client=context.wrap_socket(client,server_side=True)
+
+        threading.Thread(target=handle_client,args=(secure_client,)).start()
+
+if __name__=="__main__":
     start_server()
