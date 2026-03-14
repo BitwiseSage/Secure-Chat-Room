41171da18293fdf0156a057281bb264f5f4436d1
Arfan Asgar <md17arfan@gmail.com>
2026-03-14 09:45:57 +0530

    Added feature of private messaging between users

--- client.py
+++ client.py
@@ -1,9 +1,61 @@
 import socket
+import threading
+from protocol import send_json, recv_json
 
-HOST = "127.0.0.1"
-PORT = 5000
+HOST="127.0.0.1"
+PORT=5000
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+def receive(sock):
+    while True:
+        data=recv_json(sock)
 
-print("Connected to server")
+        if not data:
+            break
+
+        if data["type"]=="message":
+            print(f"[{data['sender']}]: {data['content']}")
+
+        elif data["type"]=="private":
+            print(f"[PRIVATE from {data['from']}]: {data['content']}")
+
+        elif data["type"]=="info":
+            print("[INFO]",data["message"])
+
+def start_client():
+
+    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect((HOST,PORT))
+
+    threading.Thread(target=receive,args=(sock,),daemon=True).start()
+
+    username=input("Enter username: ")
+    room=input("Enter room: ")
+
+    send_json(sock,{
+        "type":"join",
+        "username":username,
+        "room":room
+    })
+
+    while True:
+
+        msg=input()
+
+        if msg.startswith("/private"):
+            _,to_user,content=msg.split(" ",2)
+
+            send_json(sock,{
+                "type":"private",
+                "to":to_user,
+                "content":content
+            })
+
+        else:
+            send_json(sock,{
+                "type":"message",
+                "room":room,
+                "content":msg
+            })
+
+if __name__=="__main__":
+    start_client()
--- server.py
+++ server.py
@@ -1,21 +1,82 @@
 import socket
 import threading
+from protocol import send_json, recv_json
 
 HOST = "0.0.0.0"
 PORT = 5000
 
-rooms = {}
 clients = {}
+user_sockets = {}
+rooms = {}
+lock = threading.Lock()
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
-
-print("Server started on port", PORT)
-
-while True:
-    client, addr = server.accept()
-    print("Client connected:", addr)
+def broadcast_room(room, message, exclude=None):
+    if room in rooms:
+        for client in rooms[room]:
+            if client != exclude:
+                send_json(client, message)
 
 def handle_client(client):
-    print("Handling new client")
+    while True:
+        try:
+            data = recv_json(client)
+            if not data:
+                break
+
+            msg_type = data.get("type")
+
+            if msg_type == "join":
+                username = data["username"]
+                room = data["room"]
+
+                with lock:
+                    clients[client] = username
+                    user_sockets[username] = client
+
+                    if room not in rooms:
+                        rooms[room] = []
+                    rooms[room].append(client)
+
+                send_json(client, {"type": "info", "message": f"Joined room {room}"})
+
+            elif msg_type == "message":
+                room = data["room"]
+                username = clients.get(client)
+
+                broadcast_room(room,{
+                    "type":"message",
+                    "sender":username,
+                    "content":data["content"]
+                })
+
+            elif msg_type == "private":
+                to_user = data["to"]
+                username = clients.get(client)
+
+                if to_user in user_sockets:
+                    send_json(user_sockets[to_user],{
+                        "type":"private",
+                        "from":username,
+                        "content":data["content"]
+                    })
+
+        except:
+            break
+
+    client.close()
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+
+    print("Server running on port",PORT)
+
+    while True:
+        client, addr = server.accept()
+        print("Connection from",addr)
+
+        threading.Thread(target=handle_client,args=(client,)).start()
+
+if __name__ == "__main__":
+    start_server()
